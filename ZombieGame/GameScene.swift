415bbed2cb362d6b75f0fdf104b33d37eac4aaca
vipulgarg192 <vipulgarg192@gmail.com>
2020-06-09 16:07:29 -0400

    Zombie move on touch

--- ZombieGame/GameScene.swift
+++ ZombieGame/GameScene.swift
@@ -11,11 +11,14 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-   
+   let background = SKSpriteNode(imageNamed:"background1")
+   let zombie = SKSpriteNode(imageNamed:"zombie1")
+
+   var zombieIsTouched = false
+
     
     override func didMove(to view: SKView) {
         backgroundColor = SKColor.purple
-        let background = SKSpriteNode(imageNamed:"background1")
         // this is to bring the background into center
         background.position =   CGPoint(x: size.width/2, y: size.height/2)
 //        background.zRotation = CGFloat(M_PI) / 8
@@ -23,7 +26,6 @@ class GameScene: SKScene {
         background.zPosition = -1
         addChild(background)
         
-        let zombie = SKSpriteNode(imageNamed:"zombie1")
          // this is to bring the background into center
          zombie.position =   CGPoint(x: size.width/2, y: size.height/2)
         //background.zRotation = CGFloat(M_PI) / 8
@@ -32,6 +34,20 @@ class GameScene: SKScene {
          addChild(zombie)
         }
     
-  
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            if zombie.contains(touch.location(in: self)) {
+                zombieIsTouched = true
+            } else {
+                zombieIsTouched = false
+            }
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if (zombieIsTouched == true) {
+            zombie.position = touches.first?.location(in: self) as! CGPoint
+        }
+    }  
 }
 
