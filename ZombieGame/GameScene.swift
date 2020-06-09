0049190eb808b78c938ebe5d9d4ffb1ece81a5e5
vipulgarg192 <vipulgarg192@gmail.com>
2020-06-09 16:27:12 -0400

    Background Move with loop

--- ZombieGame/GameScene.swift
+++ ZombieGame/GameScene.swift
@@ -11,7 +11,7 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-   let background = SKSpriteNode(imageNamed:"background1")
+    var background = SKSpriteNode(imageNamed:"background1")
    let zombie = SKSpriteNode(imageNamed:"zombie1")
 
    var zombieIsTouched = false
@@ -24,7 +24,8 @@ class GameScene: SKScene {
 //        background.zRotation = CGFloat(M_PI) / 8
         background.anchorPoint = CGPoint(x: 0.5, y: 0.5) // default
         background.zPosition = -1
-        addChild(background)
+//        addChild(background)
+        createBackground()
         
          // this is to bring the background into center
          zombie.position =   CGPoint(x: size.width/2, y: size.height/2)
@@ -48,6 +49,24 @@ class GameScene: SKScene {
         if (zombieIsTouched == true) {
             zombie.position = touches.first?.location(in: self) as! CGPoint
         }
-    }  
+    }
+    
+     func createBackground() {
+              let backgroundTexture = SKTexture(imageNamed: "background1")
+
+              for i in 0 ... 1{
+                  let background = SKSpriteNode(texture: backgroundTexture)
+                  background.zPosition = -1
+                  background.anchorPoint = CGPoint(x: 0, y: 0.5)
+                  background.position = CGPoint(x:  (backgroundTexture.size().width * CGFloat(i)) - CGFloat(1 * i), y: size.height/2)
+                let moveLeft = SKAction.moveBy(x: -backgroundTexture.size().width, y: 0, duration: 20)
+                let moveReset = SKAction.moveBy(x: backgroundTexture.size().width, y: 0, duration: 0)
+                let moveLoop = SKAction.sequence([moveLeft, moveReset])
+                let moveForever = SKAction.repeatForever(moveLoop)
+
+                background.run(moveForever)
+                addChild(background)
+              }
+          }
 }
 
